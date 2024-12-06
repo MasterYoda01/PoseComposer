5fbb85d5f6293cd2d35cf4734bb1c88f2a5846f3
Cale Gregory <cgregory2500@gmail.com>
2024-12-06 07:47:10 +0000

    fixed IdentPoseDataset

--- data.py
+++ data.py
@@ -19,7 +19,7 @@ class IdentityPoseDataset(Dataset):
         self.image_width = image_width
         self.image_resolution = image_resolution
 
-        face_cascade = cv2.CascadeClassifier(
+        self.face_cascade = cv2.CascadeClassifier(
             cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
         )
 
@@ -29,13 +29,13 @@ class IdentityPoseDataset(Dataset):
         return len(self.image_paths)
 
     def _get_pose_from_image(self, image):
-        condition_image = HWC3(image)
-        detected_map, _ = self.apply_openpose(resize_image(condition_image(self.image_height)))
+        condition_image = HWC3(np.array(image))
+        detected_map, _ = self.apply_openpose(resize_image(condition_image, self.image_height))
         detected_map = HWC3(detected_map)
         
         detected_map = cv2.resize(detected_map, (512, 512), interpolation=cv2.INTER_NEAREST)
 
-        control = torch.from_numpy(detected_map.copy()).float().to(device) / 255.0
+        control = torch.from_numpy(detected_map.copy()).float().to(self.device) / 255.0
         num_samples = 1 # only using one pose
         control = torch.stack([control for _ in range(num_samples)], dim=0)
         control = einops.rearrange(control, 'b h w c -> b c h w').clone()
@@ -46,10 +46,10 @@ class IdentityPoseDataset(Dataset):
         return detected_map, control_cond, control_uncond
 
     def _get_face_from_image(self, image, padding=50):
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-        faces = face_cascade.detectMultiScale(
-            gray,
+        faces = self.face_cascade.detectMultiScale(
+            image, 
             scaleFactor=1.1,
             minNeighbors=5,
             minSize=(30, 30)
@@ -57,7 +57,9 @@ class IdentityPoseDataset(Dataset):
 
         if len(faces) == 0:
             return None
-        
+       
+        x, y, w, h = faces[0]
+
         # Get the first face (should be single object)
         x1 = max(x - padding, 0)
         y1 = max(y - padding, 0)
@@ -71,17 +73,26 @@ class IdentityPoseDataset(Dataset):
     def __getitem__(self, idx):
         image = Image.open(self.image_paths[idx])
         detected_map, pose_condition, pose_uncond = self._get_pose_from_image(image) 
-        identity_condition = self._get_face_from_image(image)    
+        identity_condition = self._get_face_from_image(np.array(image))    
 
         return image, detected_map, pose_condition, pose_uncond, identity_condition
 
 # Preview some samples
 if __name__ == "__main__":
-    ident_dataset = IdentityPoseDataset('./coco2017/test', 'cpu')
+    ident_dataset = IdentityPoseDataset('./val2017', 'cpu')
 
-    for ind in range(5):
+    def array_to_pil(array):
+
+        return Image.fromarray(np.uint8(array))
+
+    for ind in range(20):
         image, pose_map, _, _, cropped_face = ident_dataset[ind]
 
-        image.show()
-        pose_map.show()
-        cropped_face.show()
+        if cropped_face is not None:
+            pose_image = array_to_pil(pose_map)
+            ident_image = array_to_pil(cropped_face)
+
+            image.save(f'./data_example/ref_image_{ind}.jpg')
+            pose_image.save(f'./data_example/pose_image_{ind}.jpg')
+            ident_image.save(f'./data_example/ident_image_{ind}.jpg')
+           
