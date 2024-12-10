f3515514177c9a8b9cedc21ef4c218c3a040abac
juliazliu <julialiu@mit.edu>
2024-12-10 04:08:03 +0000

    completed evaluate

--- evaluate.py
+++ evaluate.py
@@ -10,7 +10,6 @@ import argparse
 import torch
 import glob
 import os
-from facenet_pytorch import MTCNN, InceptionResnetV1
 
 
 def read_reference_images(folder_path: str) -> List[np.ndarray]:
@@ -59,7 +58,7 @@ def save_image(tensor, path):
 
 
 def compute_average_similarity(
-    face_detector, face_similarity, generated_image, reference_image
+    idx, face_detector, face_similarity, generated_image, reference_image
 ) -> float:
     generated_face = face_detector(generated_image)
 
@@ -79,10 +78,9 @@ def compute_average_similarity(
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_images_per_prompt", type=int, default=4)
+    parser.add_argument("--num_images_per_prompt", type=int, default=1)
     parser.add_argument("--prediction_folder", type=str)
     parser.add_argument("--reference_folder", type=str)
-    parser.add_argument("--prompt", type=str)
 
     args = parser.parse_args()
     return args
@@ -90,7 +88,7 @@ def parse_args():
 
 def load_reference_image(reference_folder, image_id):
     path = os.path.join(reference_folder, image_id)
-    image_path = sorted(glob.glob(os.path.join(path, "*.jpeg")))[0]
+    image_path = sorted(glob.glob(os.path.join(path, "*.jpg")))[0]
     image = Image.open(image_path).convert("RGB")
     return image
 
@@ -100,6 +98,8 @@ def main():
     args = parse_args()
 
     accelerator = Accelerator()
+
+    from facenet_pytorch import MTCNN, InceptionResnetV1
 
     face_detector = MTCNN(
         image_size=160,
@@ -118,54 +118,44 @@ def main():
     text_evaluator = CLIPEvaluator(device=accelerator.device, clip_model="ViT-L/14")
 
     # get subject
-    # prompt_subject_pairs = get_combinations("", is_fastcomposer=True, split="eval")
+    prompt_subject_pairs = get_combinations("", is_fastcomposer=True, split="eval")
     image_alignments, text_alignments = [], []
-    print(os.path.join(args.prediction_folder, "*.png"))
-    images = glob.glob(os.path.join(args.prediction_folder, "*.png"))
-    print(images)
-    for image_path in images:
-        image = image_path.split("/")[-1]
-        image_id = image.split(".")[0]
-        print(image_id)
 
-        # TODO: Load reference images using image_ids from subjects
-        ref_image_path = glob.glob(os.path.join(args.reference_folder, image_id, "*.png"))[0]
-        ref_image = Image.open(ref_image_path).convert("RGB")
+    for case_id, (prompt_list, subject) in enumerate(tqdm(prompt_subject_pairs)):
+        ref_image = load_reference_image(args.reference_folder, subject)
+  
+        for prompt_id, prompt in enumerate(prompt_list):
+            for instance_id in range(args.num_images_per_prompt):
+                generated_image_path = os.path.join(
+                    args.prediction_folder,
+                    f"subject_{case_id:04d}_prompt_{prompt_id:04d}_instance_{instance_id:04d}.jpg",
+                )
+                generated_image = Image.open(generated_image_path).convert("RGB")
 
-        # for instance_id in range(args.num_images_per_prompt):
-        generated_image_path = os.path.join(args.prediction_folder, image)
-        generated_image = Image.open(generated_image_path).convert("RGB")
+                identity_similarity = compute_average_similarity(
+                    case_id, face_detector, face_similarity, generated_image, ref_image
+                )
 
-        identity_similarity = compute_average_similarity(
-            face_detector, face_similarity, generated_image, ref_image
-        )
+                generated_image_tensor = (
+                    ToTensor()(generated_image).unsqueeze(0) * 2.0 - 1.0
+                )
+                prompt_similarity = text_evaluator.txt_to_img_similarity(
+                    prompt, generated_image_tensor
+                )
 
-        generated_image_tensor = (
-            ToTensor()(generated_image).unsqueeze(0) * 2.0 - 1.0
-        )
-        prompt_similarity = text_evaluator.txt_to_img_similarity(
-            args.prompt, generated_image_tensor
-        )
+                image_alignments.append(float(identity_similarity))
+                text_alignments.append(float(prompt_similarity))
 
-        image_alignment = float(identity_similarity)
-        text_alignment = float(prompt_similarity)
-        print(f"Image Alignment: {image_alignment}")
-        print(f"Text Alignment: {text_alignment}")
-        with open(os.path.join(args.prediction_folder, "score.txt"), "w") as f:
-            f.write(f"Image Alignment: {image_alignment} Text Alignment: {text_alignment}")
-            # f.write(f"Image Alignment Std: {image_std} Text Alignment Std: {text_std}")
+    image_alignment = sum(image_alignments) / len(image_alignments)
+    text_alignment = sum(text_alignments) / len(text_alignments)
+    image_std = np.std(image_alignments)
+    text_std = np.std(text_alignments)
 
-
-    # image_alignment = sum(image_alignments) / len(image_alignments)
-    # text_alignment = sum(text_alignments) / len(text_alignments)
-    # image_std = np.std(image_alignments)
-    # text_std = np.std(text_alignments)
-
-      # print(f"Image Alignment: {image_alignment} +- {image_std}")
-      # print(f"Text Alignment: {text_alignment} +- {text_std}")
-      # with open(os.path.join(args.prediction_folder, "score.txt"), "w") as f:
-      #     f.write(f"Image Alignment: {image_alignment} Text Alignment: {text_alignment}")
-      #     f.write(f"Image Alignment Std: {image_std} Text Alignment Std: {text_std}")
+    print(f"Image Alignment: {image_alignment} +- {image_std}")
+    print(f"Text Alignment: {text_alignment} +- {text_std}")
+    with open(os.path.join(args.prediction_folder, "score.txt"), "w") as f:
+        f.write(f"Image Alignment: {image_alignment} Text Alignment: {text_alignment}\n")
+        f.write(f"Image Alignment Std: {image_std} Text Alignment Std: {text_std}\n")
 
 
 if __name__ == "__main__":
